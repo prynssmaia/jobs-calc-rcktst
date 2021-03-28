dfaeb83e2443ee12703a02e2a4e8829e6b6b699c
Prynss <prynss_lp@hotmail.com>
2021-03-27 22:53:25 -0300

    adicionado objeto ao perfil de usuario para o profile.ejs

--- src/routes.js
+++ src/routes.js
@@ -10,12 +10,22 @@ const routes = express.Router();
 
 const views = __dirname + "/views/"
 
+const profile = {
+    name: "Prynss",
+    avatar: "https://avatars.githubusercontent.com/u/71911907?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
+
 // Funções que direcionam para as páginas
 // Req - Request, Res - Response
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile"))
+routes.get('/profile', (req, res) => res.render(views + "profile", {profile: profile}))
 
 // Exportando a função rotas
 module.exports = routes;
